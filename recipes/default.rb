465d62abf9bc959e8122a8066ad714911ea0a87f
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 13:33:32 +0200

    Amended for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,11 @@
-packages = %w/
-  libtasn1-3
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid", "precise"
+  packages |= %w/
+    libtasn1-3
+  /
+end
 
 packages.each do |pkg|
   package pkg do
